33340a1a6306ca986c04d4cde22f4747e558d32a
CaduGomes <caduvitorinog@gmail.com>
2020-09-02 20:26:05 -0300

    remove node_modules

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,6 @@ const router = Router();
 
 router.post("/register", controller.register);
 router.post("/login", controller.login);
-router.post("/logout", controller.logout);
+router.post("/logout", controller.logout); 
 
 export default router;
